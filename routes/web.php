12fad27ab3642d83ab17c1e6b72865f12104f029
RicardoGoud <ricardogoud@gmail.com>
2022-08-10 23:17:10 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index');
 });
+
+Route::get('index', function () {
+    return view('products.index');
+});
+
+Route::resource('products', ProductController::class);
